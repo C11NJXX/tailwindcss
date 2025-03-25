8c4278134ff81e4220d4f983055e79c08056b701
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-25 09:53:36 +0800

    Init structure

---
+++ Project/tailwindcss-website/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+export default function App() {
+    return (
+        <>
+        {/* Header/Navigation */}
+        {/* Hero */}
+        {/* Overview */}
+        {/* About me */}
+        {/* What you'll learn */}
+        {/* What we'll build */}
+        {/* Pricing */}
+        {/* Faq */}
+        {/* Footer/Newsletter */}
+        </>
+    )
+}
